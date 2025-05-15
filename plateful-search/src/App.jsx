07ec63a3d2e549deb7510bf03f1f899897191c44
MiriamIny <miny@wits.edu>
2025-05-15 02:46:22 -0400

    updated app on home branch

--- plateful-search/src/App.jsx
+++ plateful-search/src/App.jsx
@@ -7,29 +7,20 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/item/:id" element={< Details />} /> 
+        <Route path="/about" element={< About />} />
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+  );
 }
 
 export default App
