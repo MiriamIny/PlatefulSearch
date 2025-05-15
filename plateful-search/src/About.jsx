875cad485fb7b31a05a286339fef87ab1594ba61
Hailey Lazar <hailazar38@gmail.com>
2025-05-15 17:31:11 -0400

    about us page

---
+++ plateful-search/src/About.jsx
@@ -0,0 +1,25 @@
+// src/pages/About.jsx
+import React from 'react';
+import styles from '../styles/About.module.css';
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <h2>About Plateful Search</h2>
+      <p>
+        Welcome to Plateful Search! Our mission is to help you discover delicious recipes
+        from TheMealDB API with ease and speed. Whether you’re searching for quick meals or
+        gourmet dishes, our app makes exploring culinary ideas simple and fun.
+      </p>
+      <p>
+        This project was built as part of a group assignment to practice React, React Router,
+        API fetching, and responsive design with CSS Modules.
+      </p>
+      <p>
+        We hope you enjoy using it and find inspiration for your next meal!
+      </p>
+    </div>
+  );
+};
+
+export default About;
