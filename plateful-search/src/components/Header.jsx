f93bd45f6e0343c368f59592324014ec4a971feb
Hailey Lazar <hailazar38@gmail.com>
2025-05-18 17:56:22 -0400

    added logo to header

--- plateful-search/src/components/Header.jsx
+++ plateful-search/src/components/Header.jsx
@@ -1,12 +1,13 @@
-// src/components/Header.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.logo}>Plateful Search</h1>
+      <img src={logo} alt="Plateful Search Logo" className={styles.logo} />
+      <h1>Plateful Search</h1>
       <nav className={styles.nav}>
         <NavLink to="/" className={({ isActive }) => isActive ? styles.active : ''}>
           Home
